a71a7f9a83c997f065f536b1b439a73aa1bbb94a
shehryardurrani <shehryardurrani@hotmail.co.uk>
2024-05-28 15:27:29 +0100

    read_db_creds method added for step 2 milestone 2 task 3

--- database_utils.py
+++ database_utils.py
@@ -1,3 +1,17 @@
+import yaml
+
 class DatabaseConnector:
     def __init__(self):
         pass
+
+    def read_db_creds(self):
+        try:
+            with open('db_creds.yaml', 'r') as yaml_file:
+                db_creds = yaml.safe_load(yaml_file)
+                return db_creds
+        except FileNotFoundError:
+            print("db_creds.yaml file not found. Please ensure it is created correctly.")
+            return{}
+        
+        
+        
